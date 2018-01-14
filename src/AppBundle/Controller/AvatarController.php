db70bda31bec8474e7fd1485e4cd8ede5910b51a
GrozescuRares <grozescurares@yahoo.com>
2018-01-14 16:21:48 +0200

    + avatar

---
+++ src/AppBundle/Controller/AvatarController.php
@@ -0,0 +1,193 @@
+<?php
+/**
+ * @author Lucaciu Mircea <lucaciumircea5@gmail.com>
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Utils\Functions;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use AppBundle\Entity\Rol;
+use Doctrine\DBAL\Driver\PDOException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Avatar;
+
+class AvatarController extends Controller
+{
+
+    /**
+     * @Route("/avatar/uploadAvatar", name = "upload_avatar")
+     * @Method({"POST"})
+     * @param $username
+     * @return Response
+     */
+
+    public function uploadAvatar(Request $request){
+
+        $username = $request->request->get('username');
+        $file = $request->files->get('fileName');
+        $utils = new Functions();
+
+
+        echo $file;
+        die();
+
+        if(!$username or !$file){
+            return $utils->createRespone(206, array(
+               'errors' => 'Partial data.'
+            ));
+        }
+
+        //mergem mai departe
+
+        $repoUser = $this->getDoctrine()->getManager()->getRepository(User::class);
+        $user = $repoUser->findOneBy(array(
+            'username' => $username
+        ));
+
+        $nume = explode("\\", $file);
+        $fileName = $nume[count($nume)-1];
+
+        if($user){
+
+            $repoAvatar = $this->getDoctrine()->getManager()->getRepository(Avatar::class);
+            $avatar = $repoAvatar->findOneBy(array(
+               'username' => $user,
+            ));
+
+            $em = $this->getDoctrine()->getManager();
+            if($avatar){
+                $avatar->setFile($fileName);
+                try {
+                    $em->persist($avatar);
+                    $em->flush();
+
+
+
+                    $file->move(
+                        $this->getParameter('images_location').$fileName
+                    );
+                    return $utils->createRespone(202, array(
+                       'message' => 'Avatar updated.',
+                    ));
+
+                } catch (Exception $e) {
+                    error_log($e->getMessage());
+                    return $utils->createRespone(409, array(
+                        'errors' => 'Something went wrong',
+                    ));
+                } catch (UniqueConstraintViolationException  $e) {
+                    error_log($e->getMessage());
+                    return $utils->createRespone(409, array(
+                        'errors' => 'Something went wrong',
+                    ));
+                } catch (PDOException  $e) {
+                    error_log($e->getMessage());
+                    return $utils->createRespone(409, array(
+                        'errors' => 'Something went wrong',
+                    ));
+                }
+            }
+
+
+            $newAvatar = new Avatar();
+            $newAvatar->setUsername($user);
+            $newAvatar->setFile($fileName);
+
+
+            try{
+                $em->persist($newAvatar);
+                $em->flush();
+                $file->move(
+                    $this->getParameter('images_location').$fileName
+                );
+                return $utils->createRespone(200, array(
+                   'message' => 'Avatar uploaded.'
+                ));
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+                return $utils->createRespone(409, array(
+                    'errors' => 'Something went wrong',
+                ));
+            } catch (UniqueConstraintViolationException  $e) {
+                error_log($e->getMessage());
+                return $utils->createRespone(409, array(
+                    'errors' => 'Something went wrong',
+                ));
+            } catch (PDOException  $e) {
+                error_log($e->getMessage());
+                return $utils->createRespone(409, array(
+                    'errors' => 'Something went wrong',
+                ));
+            }
+
+        }
+        else{
+            return $utils->createRespone(404, array(
+               'errors' => "There is no user with this username in the db.",
+            ));
+        }
+
+
+    }
+
+    /**
+     * @Route("/avatar/getAvatar/{username}", name = "get_avatar")
+     * @Method({"GET"})
+     * @param $username
+     * @return Response
+     */
+
+    public function getAvatar($username){
+
+        $utils = new Functions();
+
+        //mergem mai departe
+
+        $repoUser = $this->getDoctrine()->getManager()->getRepository(User::class);
+        $user = $repoUser->findOneBy(array(
+            'username' => $username
+        ));
+
+        if($user) {
+
+            $repoAvatar = $this->getDoctrine()->getManager()->getRepository(Avatar::class);
+            $avatar = $repoAvatar->findOneBy(array(
+                'username' => $user,
+            ));
+
+            if ($avatar) {
+
+
+                $filePath = $this->getParameter('images_location').$avatar->getFile();
+                $content = "<img src='$filePath' >";
+                $response = new Response();
+                $response->setContent($content);
+                return $response;
+            }
+            else{
+                return $utils->createRespone(404, array(
+                   'errors' => 'This user has no avatar yet.',
+                ));
+            }
+
+
+        }
+        else{
+            return $utils->createRespone(404, array(
+               'errors' => 'There is no user with this username in the db.',
+            ));
+        }
+    }
+
+
+
+
+}
---
+++ src/AppBundle/Entity/Avatar.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+/**
+ * Avatar
+ *
+ * @ORM\Table(name="avatar", indexes={@ORM\Index(name="user_avatar_idx", columns={"Username"})})
+ * @ORM\Entity
+ */
+class Avatar
+{
+
+
+    private $image;
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setImage(UploadedFile $image = null)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="File", type="string", length=255, nullable=false)
+     */
+    private $file;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="idavatar", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idavatar;
+
+    /**
+     * @var \AppBundle\Entity\User
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="Username", referencedColumnName="Username")
+     * })
+     */
+    private $username;
+
+    /**
+     * Avatar constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getFile(): string
+    {
+        return $this->file;
+    }
+
+    /**
+     * @param string $file
+     */
+    public function setFile(string $file)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdavatar(): int
+    {
+        return $this->idavatar;
+    }
+
+    /**
+     * @param int $idavatar
+     */
+    public function setIdavatar(int $idavatar)
+    {
+        $this->idavatar = $idavatar;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUsername(): User
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param User $username
+     */
+    public function setUsername(User $username)
+    {
+        $this->username = $username;
+    }
+
+
+
+
+}
+
