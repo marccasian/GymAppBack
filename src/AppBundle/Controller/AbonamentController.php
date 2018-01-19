1e597ad0055dd9c8be31c7d7eafdb355ef3fc017
cmarc-PC <marccasiannicolae@gmail.com>
2018-01-19 23:33:13 +0200

    + try to solve CROS problem

--- src/AppBundle/Controller/AbonamentController.php
+++ src/AppBundle/Controller/AbonamentController.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 /**
  * @author Lucaciu Mircea <lucaciumircea5@gmail.com>
  * Class AbonamentController
--- src/AppBundle/Controller/CursController.php
+++ src/AppBundle/Controller/CursController.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 /**
  * @author Casian Marc      <marccasiannicolae@gmail.com>
  * @authro Lucaciu Mircea   <lucaciumircea5@gmail.com>
